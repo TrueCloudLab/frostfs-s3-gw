7f883356d6a5abb56d4a8e448499c0ec68c0d975
Denis Kirillov <d.kirillov@yadro.com>
2022-12-16 10:47:23 +0300

    [#2] Update syncTree.sh
    Signed-off-by: Denis Kirillov <d.kirillov@yadro.com>

--- syncTree.sh
+++ syncTree.sh
@@ -6,7 +6,7 @@ REVISION="feaa9eace7098c343598bf08fb50746a1e8d2deb"
 
 echo "tree service revision ${REVISION}"
 
-FILES=$(curl -s https://github.com/nspcc-dev/neofs-node/tree/${REVISION}/pkg/services/tree | sed -n "s,.*\"/nspcc-dev/neofs-node/blob/${REVISION}/pkg/services/tree/\(.*\.pb\.go\)\".*,\1,p")
+FILES=$(curl -s https://github.com/TrueCloudLab/frostfs-node/tree/${REVISION}/pkg/services/tree | sed -n "s,.*\"/TrueCloudLab/frostfs-node/blob/${REVISION}/pkg/services/tree/\(.*\.pb\.go\)\".*,\1,p")
 
 for file in $FILES; do
   if [[ $file == *"neofs"* ]]; then
@@ -15,5 +15,5 @@ for file in $FILES; do
   else
     echo "sync '$file' in tree service"
   fi
-  curl -s "https://raw.githubusercontent.com/nspcc-dev/neofs-node/${REVISION}/pkg/services/tree/${file}" -o "./internal/neofs/services/tree/${file}"
+  curl -s "https://raw.githubusercontent.com/TrueCloudLab/frostfs-node/${REVISION}/pkg/services/tree/${file}" -o "./internal/neofs/services/tree/${file}"
 done
